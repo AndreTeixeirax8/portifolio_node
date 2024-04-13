37397c8ab8db8409fa75514d93071e49a704f750
André Teixeira <andre-x8@hotmail.com>
2024-04-13 17:44:42 -0300

    configurando o api.js

--- api.js
+++ api.js
@@ -1,2 +1,18 @@
 const express = require('express');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
+const cors  =require('cors');
+const api  = express();
+const port =3000;
+const router = express.Router();
+
+api.use(cors());
+api.use(bodyparser.urlencoded({extended:true}))
+api.use(bodyparser.json());
+
+router.get("/",(req,res)=> res.json({
+    mensagem:"api online!"
+}))
+
+api.use('/',router);
+api.listen(port)
+console.log("Servidor rodando na porta 3000")

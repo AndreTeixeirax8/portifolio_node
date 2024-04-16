bc63868a4ce20a5d0f0747f4fbb2ad6337ba4429
André Teixeira <andre-x8@hotmail.com>
2024-04-15 21:06:17 -0300

    criado a conexão com banco

--- banco/db_conexao.js
+++ banco/db_conexao.js
@@ -1,2 +1,9 @@
 const mysql = require('mysql')
-const conexao = mysql.createPool();
+const conexao = mysql.createPool({
+    host:localhost,
+    user:root,
+    password:'root',
+    database:'db_portfolio'
+});
+
+module.exports = conexao;

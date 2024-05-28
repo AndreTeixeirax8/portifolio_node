a7baa834d54f87b8691a9c2b2273cb4ecc7b8489
André Teixeira <andre-x8@hotmail.com>
2024-05-28 20:29:03 -0300

    adicionado metodo de update

--- model/portfolio/PortfolioModel.js
+++ model/portfolio/PortfolioModel.js
@@ -1,24 +1,39 @@
-const db  = require('../../banco/db_conexao');
+const db = require("../../banco/db_conexao");
 
-module.exports = class PortfolioModel{
+module.exports = class PortfolioModel {
+  static getTodos(callback) {
+    return db.query("SELECT * FROM portfolio", callback);
+  }
 
-    static getTodos(callback){
-        return db.query("SELECT * FROM portfolio",callback)
-    }
+  static getId(id, callback) {
+    return db.query(
+      "SELECT * FROM portfolio WHERE id_portfolio = ?",
+      [id],
+      callback
+    );
+  }
 
-    static getId(id,callback){
-        return db.query("SELECT * FROM portfolio WHERE id_portfolio = ?",[id],callback)
-    }
+  static adicionar(portfolio, callback) {
+    return db.query(
+      "INSERT INTO portfolio (descricao,detalhes) VALUES(?,?)",
+      [portfolio.descricao, portfolio.detalhes],
+      callback
+    );
+  }
 
-    static adicionar(portfolio,callback){
-        return db.query("INSERT INTO portfolio (descricao,detalhes) VALUES(?,?)",
-        [portfolio.descricao,portfolio.detalhes],callback)
-    }
+  static deletar(id, callback) {
+    return db.query(
+      "DELETE FROM portfolio where id_portfolio =?",
+      [id],
+      callback
+    );
+  }
 
-    static deletar(id,callback){
-        return db.query("DELETE FROM portfolio where id_portfolio =?",
-        [id],callback);
-        
-    }
-
-}
+  static editar(portfolio, callback) {
+    return db.query(
+      "UPDATE portfolio SET descricao = ?, detalhes = ? WHERE id_portfolio =?",
+      [portfolio.descricao, portfolio.detalhes, portfolio.id_portfolio],
+      callback
+    );
+  }
+};
--- router/portfolioRouter.js
+++ router/portfolioRouter.js
@@ -1,92 +1,105 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var PortfolioModel = require('../model/portfolio/PortfolioModel');
-var RespostaClass = require('../model/RespostaClass')
+var PortfolioModel = require("../model/portfolio/PortfolioModel");
+var RespostaClass = require("../model/RespostaClass");
 
-router.get('/',function(req,res,next){
+router.get("/", function (req, res, next) {
+  PortfolioModel.getTodos(function (erro, retorno) {
+    let resposta = new RespostaClass();
 
-    PortfolioModel.getTodos(function(erro,retorno){
-        let resposta = new RespostaClass();
+    if (erro) {
+      resposta.erro = true;
+      resposta.msg = "ocorreu um erro";
+      console.log("erro: ", erro);
+    } else {
+      resposta.dados = retorno;
+    }
 
-        if(erro){
-            resposta.erro = true;
-            resposta.msg ='ocorreu um erro';
-            console.log('erro: ',erro)
-
-        }else {
-            resposta.dados =retorno
-        }
-
-        res.json(resposta);
-    })
-
+    res.json(resposta);
+  });
 });
 
-router.get('/:id?',function(req,res,next){
+router.get("/:id?", function (req, res, next) {
+  PortfolioModel.getId(req.params.id, function (erro, retorno) {
+    let resposta = new RespostaClass();
 
-    PortfolioModel.getId(req.params.id,function(erro,retorno){
-        let resposta = new RespostaClass();
+    if (erro) {
+      resposta.erro = true;
+      resposta.msg = "ocorreu um erro";
+      console.log("erro: ", erro);
+      console.log("ocorreu um erro ", erro);
+    } else {
+      resposta.dados = retorno;
+    }
 
-        if(erro){
-            resposta.erro = true;
-            resposta.msg ='ocorreu um erro';
-            console.log('erro: ',erro)
-            console.log("ocorreu um erro ",erro)
-        }else {
-            resposta.dados =retorno
-        }
-
-        res.json(resposta);
-    })
-
+    res.json(resposta);
+  });
 });
 
-router.post('/?',function(req,res,next){
+router.post("/?", function (req, res, next) {
+  PortfolioModel.adicionar(req.body, function (erro, retorno) {
+    let resposta = new RespostaClass();
 
-    PortfolioModel.adicionar(req.body,function(erro,retorno){
-        let resposta = new RespostaClass();
+    if (erro) {
+      resposta.erro = true;
+      resposta.msg = "ocorreu um erro";
+      console.log("erro: ", erro);
+      console.log("ocorreu um erro ", erro);
+    } else {
+      if (retorno.affectedRows > 0) {
+        resposta.msg = "cadastro realizado";
+      } else {
+        resposta.erro = true;
+        resposta.msg = "naõ foi possivel cadastrar";
+      }
+    }
 
-        if(erro){
-            resposta.erro = true;
-            resposta.msg ='ocorreu um erro';
-            console.log('erro: ',erro)
-            console.log("ocorreu um erro ",erro)
-        }else {
-            if(retorno.affectedRows > 0){
-                resposta.msg = "cadastro realizado"
-            }else{
-                resposta.erro =true;
-                resposta.msg = 'naõ foi possivel cadastrar'
-            }
-        }
-
-        res.json(resposta);
-    })
-
+    res.json(resposta);
+  });
 });
 
-router.delete('/:id',function(req,res,next){
+router.delete("/:id", function (req, res, next) {
+  PortfolioModel.deletar(req.params.id, function (erro, retorno) {
+    let resposta = new RespostaClass();
 
-    PortfolioModel.deletar(req.params.id,function(erro,retorno){
-        let resposta = new RespostaClass();
+    if (erro) {
+      resposta.erro = true;
+      resposta.msg = "ocorreu um erro";
+      console.log("erro: ", erro);
+      console.log("ocorreu um erro ", erro);
+    } else {
+      if (retorno.affectedRows > 0) {
+        resposta.msg = "realizado com sucesso";
+      } else {
+        resposta.erro = true;
+        resposta.msg = "naõ foi possivel cadastrar";
+      }
+    }
 
-        if(erro){
-            resposta.erro = true;
-            resposta.msg ='ocorreu um erro';
-            console.log('erro: ',erro)
-            console.log("ocorreu um erro ",erro)
-        }else {
-            if(retorno.affectedRows > 0){
-                resposta.msg = "realizado com sucesso"
-            }else{
-                resposta.erro =true;
-                resposta.msg = 'naõ foi possivel cadastrar'
-            }
-        }
+    res.json(resposta);
+  });
+});
 
-        res.json(resposta);
-    })
+router.put("/", function (req, res, next) {
+  PortfolioModel.editar(req.body, function (erro, retorno) {
+    let resposta = new RespostaClass();
 
+    if (erro) {
+      resposta.erro = true;
+      resposta.msg = "ocorreu um erro";
+      console.log("erro: ", erro);
+      console.log("ocorreu um erro ", erro);
+    } else {
+      if (retorno.affectedRows > 0) {
+        resposta.msg = "realizado com sucesso";
+      } else {
+        resposta.erro = true;
+        resposta.msg = "naõ foi possivel editar";
+      }
+    }
+
+    res.json(resposta);
+  });
 });
 
 module.exports = router;
